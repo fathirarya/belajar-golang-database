9489c7fda80fb8a04aafcac09e334dad421a22fa
fathirarya <fathirarya2002@gmail.com>
2025-04-15 22:15:58 +0700

    Open Connection To Database

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
 package belajargolangdatabase
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,4 +9,13 @@ import (
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_database")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 }
