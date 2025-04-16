41b8d431ee72a63fb81719d817f59e263846598a
fathirarya <fathirarya2002@gmail.com>
2025-04-16 22:41:51 +0700

    Type Data Column

--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package belajargolangdatabase
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -43,5 +45,49 @@ func TestQerySql(t *testing.T) {
 
 		fmt.Println("Id:", id)
 		fmt.Println("Name:", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balannce, rating, created_at, birth_date, married FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balannce int32
+		var rating float64
+		var birthDate sql.NullTime
+		var created_at time.Time
+		var married bool
+
+		err := rows.Scan(&id, &name, &email, &balannce, &rating, &created_at, &birthDate, &married)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("==================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid {
+			fmt.Println("Email:", email.String)
+		}
+
+		fmt.Println("Balance:", balannce)
+		fmt.Println("Rating:", rating)
+		if birthDate.Valid {
+			fmt.Println("Birth Date:", birthDate.Time)
+		}
+		fmt.Println("CreatedAt:", created_at)
+		fmt.Println("Married:", married)
 	}
 }
