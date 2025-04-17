f36483d3da5e96b23822dd6fdeb1888a742c921d
fathirarya <fathirarya2002@gmail.com>
2025-04-17 22:37:46 +0700

    Learn Database Transaction

--- sql_test.go
+++ sql_test.go
@@ -220,3 +220,37 @@ func TestPrepareStatement(t *testing.T) {
 	}
 
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?) "
+	// do transaction
+	for i := 0; i < 10; i++ {
+		email := "fathirarya" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke" + strconv.Itoa(i)
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Comment Id", id)
+	}
+
+	err = tx.Commit() //can use rollback too
+	if err != nil {
+		panic(err)
+	}
+
+}
